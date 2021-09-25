bd57cc3a7f3115591277a4e348194c126e041ce6
“mayuri” <mayurisalunkhe01@gmail.com>
2021-09-25 09:48:41 +0530

    day 6

---
+++ temperature.sh
@@ -0,0 +1,24 @@
+read t
+read u
+case $u
+
+function celciusTofarhenite(){
+
+}
+
+function farheniteToCelcius(){
+
+}
+
+
+case $u
+
+c) if (0>t or || t>100)
+echo "
+echo "invalid input"
+else
+
+f) if(32>t || t>212)
+echo "invalid input"
+else
+
